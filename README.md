a586490e45eec5c4371f143a8cb00655e1c810f3
ThomasChan <chenjunhao5818@gmail.com>
2018-01-12 16:27:40 +0800

    First version of Hengshi Sense/Lite Document

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# GitBook Default Theme
+# Official Prodution Document Theme
 
-This is the default theme for GitBook since version `3.0.0`. It can be used as a template for theming books or can be extended.
-
-![Image](https://raw.github.com/GitbookIO/theme-default/master/preview.png)
+This is the gitbook theme for production Hengshi Sense/Lite.
--- src/js/theme/breadcrumb.js
+++ src/js/theme/breadcrumb.js
@@ -1,17 +1,61 @@
 var $ = require('jquery');
-var url = require('url');
+
+function flatArticles(articles) {
+  var mapFileNameToBreadcrumb = {};
+  function reduceMap(objs, parent) {
+    objs.forEach(function(a, index) {
+
+      var newPath = {
+        title: a.title,
+      };
+      if (a.path) {
+        newPath.path = a.path.replace('.md', '');
+      }
+      var newParent = parent.concat([newPath]);
+      if (newPath.path) {
+        mapFileNameToBreadcrumb[newPath.path] = newParent;
+      }
+      if (a.articles) {
+        reduceMap(a.articles, newParent);
+      }
+    });
+  }
+
+  reduceMap(articles, [{
+    title: '首页',
+    path: '/',
+  }]);
+  return mapFileNameToBreadcrumb;
+}
 
 function init() {
-  var $breadcrumb = $('.hengshidoc-header-breadcrumb');
-  var crumbs = window.location.pathname.slice(1).split('/');
+  var $breadcrumb = $('.hengshidoc-breadcrumb-inner');
+  var currentPath = window.location.pathname.slice(1).split('/').pop().replace('.html', '');
 
-  crumbs.forEach(function(path, index) {
+  if (!currentPath) {
     $breadcrumb.append($('<a>', {
-      text: path,
-      href: ['/'].concat(crumbs.slice(0, index)).join(''),
+      text: '首页',
+      href: '/',
     }));
-    if (index < (crumbs.length - 1)) {
+    return;
+  }
+
+  var allArticles = flatArticles(summaryObject.parts[0].articles.slice(1));
+  var current = allArticles[currentPath];
+
+  current.forEach(function(path, index) {
+    if (path.path) {
+      $breadcrumb.append($('<a>', {
+        text: path.title,
+        href: path.path === '/' ? '/' : (path.path + '.html'),
+      }));
+    } else {
       $breadcrumb.append($('<span>', {
+        text: path.title,
+      }));
+    }
+    if (index < (current.length - 1)) {
+      $breadcrumb.append($('<i>', {
         text: '/',
       }))
     }
